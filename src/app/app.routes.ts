657164c37c92f4c5c029469f6f59368c09782800
plyzie <plyzie@gmail.com>
2016-08-28 14:38:31 -0400

    Initial Project
    Seeded Project

---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import {Routes} from '@angular/router';
+import {About} from './about/about';
+import {Home} from './home/home';
+import {RepoBrowser} from './github/repo-browser/repo-browser';
+import {RepoList} from './github/repo-list/repo-list';
+import {RepoDetail} from './github/repo-detail/repo-detail';
+
+export const rootRouterConfig: Routes = [
+  {path: '', redirectTo: 'home', terminal: true},
+  {path: 'home', component: Home},
+  {path: 'about', component: About},
+];
+
